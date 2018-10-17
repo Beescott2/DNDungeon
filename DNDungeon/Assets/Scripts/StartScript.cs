254e0db9c8e7d83f18a60492849782394f29b652
Alexandre <nguyenalex@eisti.eu>
2018-10-17 16:21:04 +0200

    :tada: Joueur kinda good

---
+++ DNDungeon/Assets/Scripts/StartScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour {
+
+	public GameObject m_joueur;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (m_joueur, new Vector3(0, 3, 0), new Quaternion());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
